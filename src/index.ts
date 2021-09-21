b9e1f0d08d2d18d38ea942bce10503f3c771c59f
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-20 21:58:57 -0300

    Chicken 77 (#1)
    * Add @cig-platform/types
    
    * Add faker
    
    * Add factories

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { default as sum } from './sum';
+export { default as userFactory } from './userFactory';
+export { default as poultryAddressFactory } from './poultryAddressFactory';
+export { default as poultryFactory } from './poultryFactory';
+export { default as poultryUserFactory } from './poultryUserFactory';
