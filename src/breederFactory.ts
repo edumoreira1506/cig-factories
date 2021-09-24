acff1717ac4c86b9126125b48ee3b49342d4fb2f
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-23 21:16:45 -0300

    Rename poultry to breeder

--- src/breederFactory.ts
+++ src/breederFactory.ts
@@ -1,16 +1,16 @@
 import faker from 'faker';
-import { IPoultry } from '@cig-platform/types';
+import { IBreeder } from '@cig-platform/types';
 
-import addressFactory from './poultryAddressFactory';
+import addressFactory from './breederAddressFactory';
 
-export default function poultryFactory({
+export default function breederFactory({
   id = faker.datatype.uuid(),
   name = faker.name.findName(),
   description = faker.lorem.word(200),
   address = addressFactory(),
   active = true,
   foundationDate = faker.date.past(),
-}: Partial<IPoultry> = {}): Required<IPoultry> {
+}: Partial<IBreeder> = {}): Required<IBreeder> {
   return {
     id,
     description,
