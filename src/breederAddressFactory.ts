3166650438261b39d78288b64b8dd2fbbf3862c9
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-11 11:01:06 -0300

    Add longitude and latitude

--- src/breederAddressFactory.ts
+++ src/breederAddressFactory.ts
@@ -7,6 +7,8 @@ export default function breederAddressFactory({
   street = faker.address.streetName(),
   zipcode = `${faker.datatype.number({ min: 10000, max: 99999 })}-${faker.datatype.number({ min: 100, max: 999 })}`,
   number = faker.datatype.number(1000),
+  longitude = Number(faker.address.longitude()),
+  latitude = Number(faker.address.latitude())
 }: Partial<IBreederAddress> = {}): Required<IBreederAddress> {
   return {
     city,
@@ -14,5 +16,7 @@ export default function breederAddressFactory({
     street,
     zipcode,
     number,
+    latitude,
+    longitude,
   };
 }
