33df48e9b7caa330673c8ec26f25d32d60229da9
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-17 19:57:29 -0300

    Add poultry factory (#6)
    * Bump @cig-platform/types to 0.0.11
    
    * Add poultryFactory
    
    * Add tests
    
    * Bump to 0.0.7

---
+++ src/__tests__/poultryFactory.test.ts
@@ -0,0 +1,40 @@
+import faker from 'faker';
+
+import { poultryFactory } from '../';
+
+describe('poultryFactory', () => {
+  it('applies the id value', () => {
+    const id = faker.datatype.uuid();
+    const poultry = poultryFactory({ id });
+
+    expect(poultry.id).toBe(id);
+  });
+
+  it('applies the birthDate', () => {
+    const birthDate = faker.date.past();
+    const poultry = poultryFactory({ birthDate });
+
+    expect(poultry.birthDate).toBe(birthDate);
+  });
+
+  it('applies the color', () => {
+    const colors = {
+      eyes: faker.commerce.color(),
+      plumage: faker.commerce.color(),
+      shins: faker.commerce.color()
+    };
+    const poultry = poultryFactory({ colors });
+
+    expect(poultry.colors).toBe(colors);
+  });
+
+  it('applies the videos', () => {
+    const videos = {
+      walking: 'https://www.youtube.com/watch?v=nYXozvQaEUI',
+      presentation: 'https://www.youtube.com/watch?v=nYXozvQaEUI'
+    };
+    const poultry = poultryFactory({ videos });
+
+    expect(poultry.videos).toBe(videos);
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as breederAddressFactory } from './breederAddressFactory';
 export { default as breederFactory } from './breederFactory';
 export { default as breederUserFactory } from './breederUserFactory';
 export { default as registerFactory } from './registerFactory';
+export { default as poultryFactory } from './poultryFactory';
